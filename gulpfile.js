6912d7cc0b7862f8bc090817d15c589fe50b6b6f
Bang Nguyen <YOUR EMAIL ADDRESS>
2017-01-12 21:26:44 +0200

    update style and build

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,23 @@ var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
-var babel  = require('gulp-babel');
+var babel = require('gulp-babel');
 var historyApiFallback = require('connect-history-api-fallback');
 var concat = require('gulp-concat');
 
+
+
+// Compiles SCSS files from /scss into /css
+gulp.task('sass', function () {
+	return gulp.src(['components/**/*.scss', 'components/*.scss', 'components/**/*.css'])
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('dist'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
+});
 
 // Minify compiled CSS
 gulp.task('minify-css', ['sass'], function () {
@@ -29,16 +42,23 @@ gulp.task('minify-css', ['sass'], function () {
 		}))
 });
 
+
 // Minify JS
 gulp.task('minify-js', function () {
 	return gulp.src(['components/**/*.js', 'components/*.js'])
 		.pipe(sourcemaps.init())
-		.pipe(babel({presets: ['es2016']}))
-		.pipe(babel({presets: ['es2015']}))
-		.pipe(uglify().on('error', function(e){
-         console.log(e);
-    }))
-		.pipe(rename({suffix: '.min' }))
+		.pipe(babel({
+			presets: ['es2016']
+		}))
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(uglify().on('error', function (e) {
+			console.log(e);
+		}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('dist'))
 		.pipe(browserSync.reload({
@@ -46,10 +66,11 @@ gulp.task('minify-js', function () {
 		}))
 });
 
-gulp.task('scripts', function() {
-  return gulp.src('./dist/**/*.js')
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('./dist'));
+// Combine js
+gulp.task('scripts', ['minify-js'], function () {
+	return gulp.src('./dist/**/*.js')
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('./dist'));
 });
 
 
@@ -73,37 +94,26 @@ gulp.task('copy', function () {
 })
 
 // Run everything
-gulp.task('default', ['minify-css', 'minify-js','scripts', 'copy']);
+gulp.task('default', ['minify-css', 'scripts', 'copy']);
+
 
 // Configure the browserSync task
 gulp.task('browserSync', function () {
 	browserSync.init({
 		server: {
 			baseDir: '.',
-			middleware: [ historyApiFallback() ]
+			middleware: [historyApiFallback()]
 		},
-		port:3000
+		port: 3000
 	})
 })
 
 // Dev task with browserSync
 gulp.task('dev', ['browserSync', 'sass', 'minify-js', 'scripts'], function () {
-	gulp.watch(['components/**/*.scss','components/*.scss'], ['sass']);
+	gulp.watch(['components/**/*.scss', 'components/*.scss'], ['sass']);
 	gulp.watch('components/**/*.js', ['minify-js']);
 	gulp.watch('components/*.js', ['minify-js']);
 	// Reloads the browser whenever HTML or JS files change
-	gulp.watch(['*.html','components/**/*.html' ], browserSync.reload);
+	gulp.watch(['*.html', 'components/**/*.html'], browserSync.reload);
 	gulp.watch('components/**/*.js', browserSync.reload);
-});
-
-// Compiles SCSS files from /scss into /css
-gulp.task('sass', function () {
-	return gulp.src(['components/**/*.scss', 'components/*.scss', 'components/**/*.css'])
-		.pipe(sourcemaps.init())
-		.pipe(sass().on('error', sass.logError))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('dist'))
-		.pipe(browserSync.reload({
-			stream: true
-		}))
 });
